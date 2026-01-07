3b4b593f3201463d52fb94ca18275faef82d2626
Daniel <danielp4p1@gmail.com>
2026-01-07 11:35:42 -0500

    commit

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo03.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo03.java
@@ -1,4 +1,4 @@
-/*
+                                                      /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,7 +9,7 @@ package ejemplos01;
  *
  * @author reroes
  */
-public class Ejemplo03 {
+public class Ejemplo03 { 
 
     public static void main(String[] args) {
         // 
