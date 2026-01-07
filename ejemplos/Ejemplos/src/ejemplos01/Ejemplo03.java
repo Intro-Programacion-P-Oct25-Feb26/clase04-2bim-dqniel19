6ca1cf048485a035f4cb2773135b0122e681a765
Daniel <danielp4p1@gmail.com>
2026-01-07 11:50:49 -0500

    commit

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo03.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo03.java
@@ -1,4 +1,4 @@
-                                                      /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,7 +9,7 @@ package ejemplos01;
  *
  * @author reroes
  */
-public class Ejemplo03 { 
+public class Ejemplo03 {
 
     public static void main(String[] args) {
         // 
@@ -17,6 +17,8 @@ public class Ejemplo03 {
                                          // obtenerSuma
         System.out.printf("El valor de la suma es: %d\n", valor);
         
+        System.out.printf(suma);
+        // No es posible imprimir debido a que la variable suma no existe en esta función
         // System.out.printf("El valor de la suma es: %d\n", obtenerSuma(10, 30));
     }
     
